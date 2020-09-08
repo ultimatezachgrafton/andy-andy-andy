f5f06a5d101e94112108da8cd4ec3239d14b9747
ultimatezachgrafton <arrowny@aol.com>
2020-09-07 22:37:23 -0700

    Add files via upload

--- dom.js
+++ dom.js
@@ -1,13 +1,13 @@
-var slackAndy = document.getElementById('slack-andy').addEventListener('click', run); // rock
-var githubAndy = document.getElementById('github-andy').addEventListener('click', run); // paper
-var quarantineAndy = document.getElementById('quarantine-andy').addEventListener('click', run); // scissors
+const slackAndy = document.getElementById('slack-andy').addEventListener('click', run); // rock
+const githubAndy = document.getElementById('github-andy').addEventListener('click', run); // paper
+const quarantineAndy = document.getElementById('quarantine-andy').addEventListener('click', run); // scissors
 
-var computerChoice;
-var playerChoice;
+let computerChoice;
+let playerChoice;
 
-var playerAnnouncement = document.getElementById('playerAnnouncement');
-var computerAnnouncement = document.getElementById('computerAnnouncement');
-var result = document.getElementById('result');
+const playerAnnouncement = document.getElementById('playerAnnouncement');
+const computerAnnouncement = document.getElementById('computerAnnouncement');
+const result = document.getElementById('result');
 
 function run() {
     playerChoice = this.innerText;
@@ -16,36 +16,33 @@ function run() {
 }
 
 function computerTurn() {
-  var random = Math.floor(Math.random() * 3);
+  const random = Math.floor(Math.random() * 3);
   if (random === 0) {
-      computerChoice = "Slack Andy";
-  } else if (random === 1) {
-      computerChoice = "Github Andy";
-  } else {
-      computerChoice = "Quarantine Andy";
+      return computerChoice = "Slack Andy";
+  } 
+  if (random === 1) {
+      return computerChoice = "Github Andy";
   }
+  computerChoice = "Quarantine Andy";
 }
 
 function declareVictor() {
     playerAnnouncement.innerHTML = "YOU HAVE CHOSEN: " + playerChoice;
     computerAnnouncement.innerHTML = "YOUR FRIEND <b>THE COMPUTER</b> HAS CHOSEN: " + computerChoice;
 
-    if (playerChoice == computerChoice) {
-        result.innerHTML = "YOU AND YOUR FRIEND HAVE TIED. IT IS UNSATISFYING.";
-        
-    } else if (playerChoice === "Slack Andy" && computerChoice === "Github Andy") {
-        result.innerHTML = "YOU CHOSE POORLY. YOUR LIFE IS FORFEIT.";
-    } else if (playerChoice === "Slack Andy" && computerChoice === "Quarantine Andy") {
-        result.innerHTML = "WITH THIS VICTORY YOU HAVE STAVED OFF THE SINGULARITY FOR ANOTHER PRECIOUS NANOSECOND.";
+    const slackAndyLosesToGithubAndy = playerChoice === "Slack Andy" && computerChoice === "Github Andy";
+    const githubAndyLosesToQuarantineAndy = playerChoice === "Github Andy" && computerChoice === "QuaantineAndy"
+    const quarantineAndyLosesToSlackAndy = playerChoice === "Quarantine Andy" && computerChoice === "Slack Andy"
 
-    } else if (playerChoice === "Github Andy" && computerChoice === "Slack Andy") {
-        result.innerHTML = "WITH THIS VICTORY YOU HAVE STAVED OFF THE SINGULARITY FOR ANOTHER PRECIOUS NANOSECOND.";
-    } else if (playerChoice === "Github Andy" && computerChoice === "Quarantine Andy") {
-        result.innerHTML = "YOU CHOSE POORLY. YOUR LIFE IS FORFEIT."; 
-
-    } else if (playerChoice === "Quarantine Andy" && computerChoice === "Slack Andy") {
-        result.innerHTML = "YOU CHOSE POORLY. YOUR LIFE IS FORFEIT.";
-    } else if (playerChoice === "Quarantine Andy" && computerChoice === "Github Andy") {
-        result.innerHTML = "WITH THIS VICTORY YOU HAVE STAVED OFF THE SINGULARITY FOR ANOTHER PRECIOUS NANOSECOND.";
+    if (slackAndyLosesToGithubAndy || githubAndyLosesToQuarantineAndy || quarantineAndyLosesToSlackAndy) {
+        return result.innerHTML = "YOU CHOSE POORLY. YOUR LIFE IS FORFEIT.";
     }
+
+    if ((playerChoice === "Slack Andy" && computerChoice === "Quarantine Andy")
+        || (playerChoice === "Github Andy" && computerChoice === "Quarantine Andy")
+        || (playerChoice === "Quarantine Andy" && computerChoice === "Github Andy")) {
+            return result.innerHTML = "WITH THIS VICTORY YOU HAVE STAVED OFF THE SINGULARITY FOR ANOTHER PRECIOUS NANOSECOND.";
+        }
+
+    result.innerHTML = "YOU AND YOUR FRIEND HAVE TIED. IT IS UNSATISFYING.";
 }
